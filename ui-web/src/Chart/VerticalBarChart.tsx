88ccae0a4d0393861cf4828b7df5f0c9ef13697f
Konrad Mikucki <komik966@gmail.com>
2020-04-24 20:18:01 +0200

    [wip] chart

--- ui-web/src/Chart/VerticalBarChart.tsx
+++ ui-web/src/Chart/VerticalBarChart.tsx
@@ -4,11 +4,17 @@ import * as d3 from 'd3';
 const VerticalBarChart: FC<Props> = ({ data, color, height }) => {
   return (
     <div
-      ref={elem => {
+      ref={(elem) => {
         if (!elem) {
           return;
         }
-        const svg = chart(data, elem.offsetWidth, height, color);
+        const svg = chart(
+          Object.keys(data),
+          Object.values(data),
+          elem.offsetWidth,
+          height,
+          color,
+        );
         if (!svg) {
           return;
         }
@@ -20,72 +26,68 @@ const VerticalBarChart: FC<Props> = ({ data, color, height }) => {
 };
 
 const chart = (
-  dataRemote: Props['data'],
+  xData: string[],
+  yData: number[],
   width: number,
   height: number,
   color: string,
 ) => {
-  const margin = { top: 30, right: 0, bottom: 30, left: 40 };
-  const data = Object.assign(
-    Object.keys(dataRemote).reduce(
-      (prev, curr) => [...prev, { name: curr, value: dataRemote[curr] }],
-      [],
-    ),
-    { format: 'kB', y: 'Rozmiar' },
-  );
-  const yAxis = g =>
-    g
-      .attr('transform', `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y).ticks(null, data.format))
-      .call(g => g.select('.domain').remove())
-      .call(g =>
-        g
-          .append('text')
-          .attr('x', -margin.left)
-          .attr('y', 10)
-          .attr('fill', 'currentColor')
-          .attr('text-anchor', 'start')
-          .text(data.y),
-      );
-  const xAxis = g =>
-    g.attr('transform', `translate(0,${height - margin.bottom})`).call(
-      d3
-        .axisBottom(x)
-        .tickFormat(i => data[i].name)
-        .tickSizeOuter(0),
-    );
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, d => d.value)])
-    .nice()
-    .range([height - margin.bottom, margin.top]);
+  console.log(xData, yData);
+  const margin = { bottom: 30, left: 40, right: 0, top: 30 };
 
   const x = d3
     .scaleBand()
-    .domain(d3.range(data.length))
+    .domain(xData)
     .range([margin.left, width - margin.right])
     .padding(0.1);
 
+  const y = d3
+    .scaleLinear()
+    .domain([d3.min(yData) || 0, d3.max(yData) || 0])
+    .nice()
+    .range([height - margin.bottom, margin.top]);
+
   const svg = d3.create('svg').attr('viewBox', `0, 0, ${width}, ${height}`);
+
+  // svg
+  //   .append('g')
+  //   .attr('fill', color)
+  //   .selectAll('rect')
+  //   .data(yData)
+  //   .join('rect')
+  //   .attr('x', (d, i) => x(i))
+  //   .attr('y', (d) => y(d.value))
+  //   .attr('height', (d) => y(0) - y(d.value))
+  //   .attr('width', x.bandwidth());
 
   svg
     .append('g')
-    .attr('fill', color)
-    .selectAll('rect')
-    .data(data)
-    .join('rect')
-    .attr('x', (d, i) => x(i))
-    .attr('y', d => y(d.value))
-    .attr('height', d => y(0) - y(d.value))
-    .attr('width', x.bandwidth());
-  svg.append('g').call(xAxis);
-  svg.append('g').call(yAxis);
+    .call((g) =>
+      g
+        .attr('transform', `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x)),
+    );
+  svg.append('g').call(
+    (g) =>
+      g.attr('transform', `translate(${margin.left},0)`).call(d3.axisLeft(y)),
+    // .call((g) => g.select('.domain').remove())
+    // .call(
+    //   (g) =>
+    //     g
+    //       .append('text')
+    //       .attr('x', -margin.left)
+    //       .attr('y', 10)
+    //       .attr('fill', 'currentColor')
+    //       .attr('text-anchor', 'start'),
+    //   // .text(data),
+    // ),
+  );
 
   return svg.node();
 };
 
 interface Props {
-  data: Record<number | string, number | string>;
+  data: Record<string, number>;
   color: string;
   height: number;
 }
